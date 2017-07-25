0244efde2dd2cedea82fdaa9a14d9f4f4203f918
kanbin <kanbin@live.com>
2017-07-25 14:26:59 +0800

    动态变化的圆圈

---
+++ app/src/main/java/com/example/myview/CircleView.java
@@ -0,0 +1,46 @@
+package com.example.myview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by SONY on 2017/7/24.
+ */
+
+public class CircleView extends View {
+    private Paint mPaint;
+    private int radius;
+
+    public CircleView(Context context) {
+        this(context, null);
+    }
+
+    public CircleView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(200, 200, radius, mPaint);
+    }
+
+    public void setRadius(int radius){
+        this.radius = radius;
+        invalidate();
+    }
+}
--- app/src/main/java/com/example/myview/MainActivity.java
+++ app/src/main/java/com/example/myview/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.myview;
 
+import android.os.Handler;
+import android.os.Message;
+import android.renderscript.RenderScript;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private int radius = 0;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mCircleView.setRadius(radius);
+        }
+    };
+    private CircleView mCircleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCircleView = (CircleView) findViewById(R.id.circle_view);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        while (radius >= 200) {
+                            radius = 0;
+                        }
+                        mHandler.sendEmptyMessage(0);
+                        radius++;
+                        Thread.sleep(40);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+
+                    }
+                }
+            }
+
+        }).start();
     }
 }
